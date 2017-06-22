71384094b372c3c69c0d565d599da3fe4fc395f8
Thomson <779387850@qq.com>
2017-06-22 23:45:00 +0800

    播放音乐

--- song.js
+++ song.js
@@ -16,7 +16,14 @@ $(function() {
             if (!object) { return }
             $p = $('<p></p>')
             $p.attr('data-time', object.time).text(object.words)
-            $p.appendTo($lyric)
+            $p.appendTo($lyric.children('.lines'))
         })
     })
+
+    let audio = document.createElement('audio')
+    audio.src = 'http://win.web.ra03.sycdn.kuwo.cn/2309f752a2c67aafad8991d5882afce0/594be474/resource/a3/48/67/14/4247427266.aac'
+    audio.oncanplay = function() {
+        // audio.play()
+        $('.disc-container').addClass('playing')
+    }
 })
